54f4c25b87b723ae2b177da47747d87b050ea02f
Belen JN <belenjarabanavas31@gmail.com>
2022-12-09 11:58:49 +0100

    Drawer installed and created

---
+++ src/navigator/BasicLateralMenu.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {SettingsScreen} from '../screens/SettingsScreen';
+import {StackNavigator} from './StackNavigator';
+
+const Drawer = createDrawerNavigator();
+
+export const BasicLateralMenu = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+    </Drawer.Navigator>
+  );
+};
