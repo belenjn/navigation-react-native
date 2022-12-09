aa0e2af2d5c99a46364bcefa55041b503d92b897
Belen JN <belenjarabanavas31@gmail.com>
2022-12-09 10:41:54 +0100

    Stack Navigation installed

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {Text} from 'react-native';
 
