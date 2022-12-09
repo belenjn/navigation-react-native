1277797e98827a49728190c90428f20c0385f8f7
Belen JN <belenjarabanavas31@gmail.com>
2022-12-09 15:26:01 +0100

    Refactoring

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope */
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
+// import {BasicLateralMenu} from './src/navigator/BasicLateralMenu';
 import {StackNavigator} from './src/navigator/StackNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
       <StackNavigator />
+      {/* <BasicLateralMenu /> */}
     </NavigationContainer>
   );
 };
