bba6d7c0a510316df26506d84911fe6ad50b4b14
Belen JN <belenjarabanavas31@gmail.com>
2022-12-09 10:30:42 +0100

    Installations and deleted code in App

---
+++ App.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text> App</Text>
+    </View>
+  );
+};
