e9111d5cf394feb4ce2dcfd84ac34ebdd80da463
Belen JN <belenjarabanavas31@gmail.com>
2022-12-12 11:44:41 +0100

    Tabs's names changed

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -9,9 +9,9 @@ const Tab = createBottomTabNavigator();
 export const Tabs = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Tab1Screen} />
-      <Tab.Screen name="Home" component={Tab2Screen} />
-      <Tab.Screen name="Home" component={Tab3Screen} />
+      <Tab.Screen name="Tab1Screen" component={Tab1Screen} />
+      <Tab.Screen name="Tab2Screen" component={Tab2Screen} />
+      <Tab.Screen name="Tab3Screen" component={Tab3Screen} />
     </Tab.Navigator>
   );
 };
