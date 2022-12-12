433a443827d286c57dc72aa81312eb0bf4fba241
Belen JN <belenjarabanavas31@gmail.com>
2022-12-12 15:38:15 +0100

    Icons for ios and some icons added in Tab1Screed

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {useEffect} from 'react';
-import {styles} from '../theme/appTheme';
+import {styles, colors} from '../theme/appTheme';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const Tab1Screen = () => {
+  const {top} = useSafeAreaInsets();
+
   useEffect(() => {
     console.log('Tab 1 screen');
   }, []);
@@ -12,7 +15,42 @@ export const Tab1Screen = () => {
   return (
     <View style={styles.globalMargin}>
       <Text style={styles.title}>Icons</Text>
-      <Icon name="airplane-outline" size={30} />
+      <Icon
+        name="airplane-outline"
+        size={80}
+        color={colors.primary}
+        style={{...styles.globalMargin, marginTop: top}}
+      />
+      <Icon
+        name="attach-outline"
+        size={80}
+        color={colors.primary}
+        style={{...styles.globalMargin, marginTop: top}}
+      />
+      <Icon
+        name="bonfire-outline"
+        size={80}
+        color={colors.primary}
+        style={{...styles.globalMargin, marginTop: top}}
+      />
+      <Icon
+        name="calculator-outline"
+        size={80}
+        color={colors.primary}
+        style={{...styles.globalMargin, marginTop: top}}
+      />
+      <Icon
+        name="flower-outline"
+        size={80}
+        color={colors.primary}
+        style={{...styles.globalMargin, marginTop: top}}
+      />
+      <Icon
+        name="leaf-outline"
+        size={80}
+        color={colors.primary}
+        style={{...styles.globalMargin, marginTop: top}}
+      />
     </View>
   );
 };
