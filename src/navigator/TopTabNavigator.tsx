2cb402a0031683bd590520533e391cf51e81ea12
Belen JN <belenjarabanavas31@gmail.com>
2022-12-12 14:32:05 +0100

    New screens for TopTabNavigator

--- src/navigator/TopTabNavigator.tsx
+++ src/navigator/TopTabNavigator.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {ChatScreen} from '../screens/ChatScreen';
+import {ContactsScreen} from '../screens/ContactsScreen';
+import {AlbumsScreen} from '../screens/AlbumsScreen';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
   return (
     <Tab.Navigator>
-      {/* <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+      <Tab.Screen name="ChatScreen" component={ChatScreen} />
+      <Tab.Screen name="ContactScreen" component={ContactsScreen} />
+      <Tab.Screen name="AlbumsScreen" component={AlbumsScreen} />
     </Tab.Navigator>
   );
 };
